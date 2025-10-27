5d6358507fc44119b2590d4d9174d319c42ccecc
evolentDelight <arkanumbk@gmail.com>
2025-10-26 20:13:07 -0700

    initial commit

---
+++ server.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/')
+def index():
+  return render_template('index.html')
+
+@app.route('/api/submit', methods=['POST'])
+def submit_data():
+  data = request.get_json()
+
+  print(data)
+
+if __name__ == '__main__':
+  app.run(debug=True)
