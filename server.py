1fad10ccde624364cd416d1532072fffeafa805d
evolentDelight <arkanumbk@gmail.com>
2025-10-26 22:47:56 -0700

    Added locale time for less confusion

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 import socket
 import yfinance as yf
+import locale
 import datetime as dt
 # Main Functionality methods
 
@@ -63,8 +64,9 @@ def handler(ticker_symbol):
   percentage = calculate_percentage(close, open)
 
   now = dt.datetime.now()
+  locale.setlocale(locale.LC_ALL, "en_US.UTF-8")
 
-  return f"""Current Date and Time: {now.strftime('%a %b %d %I:%S')}
+  return f"""Current Date and Time: {now.strftime('%a %b %d %I:%S UTF-8')}
 Company Name: {company_name} ({symbol})
 Stock Price: {current_price} {value} ({percentage}%)
 """
